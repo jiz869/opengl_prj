85e15a93bc3ba73f9206aad13b03ff4c4606678c
jiz869 <freeprogrammer@gmail.com>
2014-02-17 22:28:47 -0500

    first version

---
+++ Main.cpp
@@ -0,0 +1,279 @@
+#include "Framework.h"
+#include "Shader.h"
+
+#define MODEL_PATH "models/teapot.3ds"
+
+
+// Note: See the SMFL documentation for info on setting up fullscreen mode
+// and using rendering settings
+// http://www.sfml-dev.org/tutorials/1.6/window-window.php
+sf::WindowSettings settings(24, 8, 2);
+sf::Window window(sf::VideoMode(800, 600), "CS248 Rules!", sf::Style::Close, settings);
+ 
+// This is a clock you can use to control animation.  For more info, see:
+// http://www.sfml-dev.org/tutorials/1.6/window-time.php
+sf::Clock clck;
+
+// This creates an asset importer using the Open Asset Import library.
+// It automatically manages resources for you, and frees them when the program
+// exits.
+Assimp::Importer importer;
+const aiScene* scene;
+struct aiVector3D scene_min, scene_max, scene_center;
+
+#define aisgl_min(x,y) (x<y?x:y)
+#define aisgl_max(x,y) (y>x?y:x)
+
+void initOpenGL();
+void loadAssets();
+void handleInput();
+void renderFrame();
+
+int main(int argc, char** argv) {
+
+    initOpenGL();
+    loadAssets();
+
+    // Put your game loop here (i.e., render with OpenGL, update animation)
+    while (window.IsOpened()) {
+
+        handleInput();
+        renderFrame();
+        window.Display();
+    }
+
+    return 0;
+}
+
+
+
+void initOpenGL() {
+    // Initialize GLEW on Windows, to make sure that OpenGL 2.0 is loaded
+#ifdef FRAMEWORK_USE_GLEW
+    GLint error = glewInit();
+    if (GLEW_OK != error) {
+        std::cerr << glewGetErrorString(error) << std::endl;
+        exit(-1);
+    }
+    if (!GLEW_VERSION_2_0 || !GL_EXT_framebuffer_object) {
+        std::cerr << "This program requires OpenGL 2.0 and FBOs" << std::endl;
+        exit(-1);
+    }
+#endif
+
+    // This initializes OpenGL with some common defaults.  More info here:
+    // http://www.sfml-dev.org/tutorials/1.6/window-opengl.php
+    glClearDepth(1.0f);
+    glClearColor(0.15f, 0.15f, 0.15f, 0.15f);
+    glEnable(GL_DEPTH_TEST);
+
+   	const double aspectRatio = ((float) window.GetWidth() / (float)window.GetHeight()), fieldOfView = 45.0;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(fieldOfView, aspectRatio, 1.0, 1000.0);  /* Znear and Zfar */
+
+    glViewport(0, 0, window.GetWidth(), window.GetHeight());
+}
+
+
+// ----------------------------------------------------------------------------
+void get_bounding_box_for_node (const struct aiNode* nd, 
+	struct aiVector3D* min, 
+	struct aiVector3D* max, 
+	struct aiMatrix4x4* trafo
+){
+	struct aiMatrix4x4 prev;
+	unsigned int n = 0, t;
+
+	prev = *trafo;
+	aiMultiplyMatrix4(trafo,&nd->mTransformation);
+
+	for (; n < nd->mNumMeshes; ++n) {
+		const struct aiMesh* mesh = scene->mMeshes[nd->mMeshes[n]];
+		for (t = 0; t < mesh->mNumVertices; ++t) {
+
+			struct aiVector3D tmp = mesh->mVertices[t];
+			aiTransformVecByMatrix4(&tmp,trafo);
+
+			min->x = aisgl_min(min->x,tmp.x);
+			min->y = aisgl_min(min->y,tmp.y);
+			min->z = aisgl_min(min->z,tmp.z);
+
+			max->x = aisgl_max(max->x,tmp.x);
+			max->y = aisgl_max(max->y,tmp.y);
+			max->z = aisgl_max(max->z,tmp.z);
+		}
+	}
+
+	for (n = 0; n < nd->mNumChildren; ++n) {
+		get_bounding_box_for_node(nd->mChildren[n],min,max,trafo);
+	}
+	*trafo = prev;
+}
+
+// ----------------------------------------------------------------------------
+void get_bounding_box (struct aiVector3D* min, struct aiVector3D* max)
+{
+	struct aiMatrix4x4 trafo;
+	aiIdentityMatrix4(&trafo);
+
+	min->x = min->y = min->z =  1e10f;
+	max->x = max->y = max->z = -1e10f;
+	get_bounding_box_for_node(scene->mRootNode,min,max,&trafo);
+}
+
+void loadAssets() {
+    // Read in an asset file, and do some post-processing.  There is much 
+    // more you can do with this asset loader, including load textures.
+    // More info is here:
+    // http://assimp.sourceforge.net/lib_html/usage.html
+    scene = importer.ReadFile(MODEL_PATH,  
+        aiProcess_CalcTangentSpace |
+        aiProcess_Triangulate |
+        aiProcess_JoinIdenticalVertices |
+        aiProcessPreset_TargetRealtime_Quality);
+
+    if (!scene || scene->mNumMeshes <= 0) {
+        std::cerr << importer.GetErrorString() << std::endl;
+        exit(-1);
+    }
+
+    //get scale
+    get_bounding_box(&scene_min, &scene_max);
+    scene_center.x = (scene_min.x + scene_max.x)/2.0f;
+    scene_center.y = (scene_min.y + scene_max.y)/2.0f;
+    scene_center.z = (scene_min.z + scene_max.z)/2.0f;
+        
+    //////////////////////////////////////////////////////////////////////////
+    // TODO: LOAD YOUR SHADERS/TEXTURES
+    //////////////////////////////////////////////////////////////////////////
+}
+
+
+
+
+void handleInput() {
+    //////////////////////////////////////////////////////////////////////////
+    // TODO: ADD YOUR INPUT HANDLING HERE. 
+    //////////////////////////////////////////////////////////////////////////
+
+    // Event loop, for processing user input, etc.  For more info, see:
+    // http://www.sfml-dev.org/tutorials/1.6/window-events.php
+    sf::Event evt;
+    while (window.GetEvent(evt)) {
+        switch (evt.Type) {
+        case sf::Event::Closed: 
+            // Close the window.  This will cause the game loop to exit,
+            // because the IsOpened() function will no longer return true.
+            window.Close(); 
+            break;
+        case sf::Event::Resized: 
+            // If the window is resized, then we need to change the perspective
+            // transformation and viewport
+            glViewport(0, 0, evt.Size.Width, evt.Size.Height);
+            break;
+        default: 
+            break;
+        }
+    }
+}
+
+
+void renderNode(const struct aiScene *sc, const struct aiNode *nd)
+{
+    struct aiMatrix4x4 m = nd->mTransformation;
+    unsigned int i;
+    unsigned int f;
+    unsigned int v;
+
+    //update transform
+    aiTransposeMatrix4(&m); //assimp matrix is row major. Need transpose for gl
+    glPushMatrix();
+    glMultMatrixf((float*)&m);
+
+    //draw this node
+    for(i=0; i < nd->mNumMeshes; ++i) {
+        const struct aiMesh *mesh = sc->mMeshes[ nd->mMeshes[i] ];
+        if( mesh->HasNormals() ) {
+            glEnable(GL_LIGHTING);
+        }else{
+            glDisable(GL_LIGHTING);
+        }
+
+        for(f=0; f < mesh->mNumFaces; ++f) {
+            const struct aiFace *face = &mesh->mFaces[f];
+            //GLenum face_mode;
+
+            /*
+            switch(face->mNumIndices) {
+                case 1: face_mode = GL_POINTS; break;
+                case 2: face_mode = GL_LINES; break;
+                case 3: face_mode = GL_TRIANGLES; break;
+            }
+            */
+            glBegin(GL_TRIANGLES);
+            for(v=0; v < face->mNumIndices; ++v) {
+                int index = face->mIndices[v];
+                if(mesh->mNormals != NULL) {
+                    glNormal3fv(&mesh->mNormals[index].x);
+                }
+                glVertex3fv(&mesh->mVertices[index].x);
+            }
+
+            glEnd();
+
+        }
+    }
+
+    //draw all children
+    unsigned int n;
+    for(n=0; n < nd->mNumChildren; ++n) {
+        renderNode( sc, nd->mChildren[n]); 
+    }
+
+    glPopMatrix();
+
+}
+
+
+void renderFrame() {
+    //////////////////////////////////////////////////////////////////////////
+    // TODO: ADD YOUR RENDERING CODE HERE.  You may use as many .cpp files 
+    // in this assignment as you wish.
+    //////////////////////////////////////////////////////////////////////////
+    float tmp;
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);    // uses default lighting parameters
+    glColorMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.f, 0.f,8.f,0.f,0.f,0.0f,0.f,1.f,0.f);
+    //gluLookAt(0.f, 2.f,3.f,0.f,0.f,0.f,0.f,1.f,-1.f);
+
+
+    // scale the whole asset to fit into our view frustum 
+	tmp = scene_max.x-scene_min.x;
+	tmp = aisgl_max(scene_max.y - scene_min.y,tmp);
+	tmp = aisgl_max(scene_max.z - scene_min.z,tmp);
+	tmp = 1.f / tmp;
+	//glScalef(tmp, tmp, tmp);
+
+    //center the model
+
+    glRotatef(90, 0.0f, 1.0f, 0.0f);
+    glRotatef(90, 1.0f, 0.0f, 0.0f);
+    //glTranslatef(-scene_center.x, -scene_center.y, -scene_center.z);    //move center to the origin
+    renderNode(scene, scene->mRootNode);
+
+}
+
+
+
+
+
+
+
+
